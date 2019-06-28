e2036878d8954daaa2dc40b2b59b55738e931f11
Jayant Rai <jayant.chamling@outlook.com>
2019-06-28 03:22:32 -0700

    added displayItem() to show in the terminal

--- bamazonCustomer.js
+++ bamazonCustomer.js
@@ -12,4 +12,23 @@ const connection = mysql.createConnection({
 connection.connect(function (err) {
   if (err) throw err
   console.log('Connected as id ' + connection.threadId)
+  // call displayItems here when connected
+  displayItems()
 })
+
+function displayItems () {
+  var sql = 'SELECT * FROM Products'
+  connection.query(sql, function (err, rows, fields) {
+    if (err) throw err
+
+    // loop through mysql database for each item
+    for (var i = 0; i < rows.length; i++) {
+      console.log('SKU#: ' + rows[i].sku + '|' +
+        'Product: ' + rows[i].product_name + '|' +
+        'Department: ' + rows[i].department_name + '|' +
+        'Price: ' + '$' + rows[i].price + '|' +
+        'In Stock: ' + rows[i].stock_quantity)
+    }
+  }
+  )
+}
